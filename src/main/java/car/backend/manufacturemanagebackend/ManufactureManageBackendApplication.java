12fbe158a47bbd636cfee8e63b3162d99b265661
Bui Vu <buivu120597@gmail.com>
2024-07-31 10:56:39 +0700

    first commit

---
+++ src/main/java/car/backend/manufacturemanagebackend/ManufactureManageBackendApplication.java
@@ -0,0 +1,13 @@
+package car.backend.manufacturemanagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManufactureManageBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ManufactureManageBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/car/backend/manufacturemanagebackend/ManufactureManageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package car.backend.manufacturemanagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManufactureManageBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
